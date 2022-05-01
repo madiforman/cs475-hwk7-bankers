a56837108c857c61782a9e876d12ab0a067e88cb
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-05-01 15:37:37 -0400

    finishing touches

--- vector.h
+++ vector.h
@@ -2,10 +2,12 @@
 /*** Globals ***/
 extern int N_RES;
 extern int N_PROC;
-/*** Function Prototypes of Vector and Matrix Operations ***/
+/*** Function Prototypes ***/
+int **read_mtrx(FILE *fp, char mode);
 void sub_mtrx(int **MA, int **MB, int **MC);
+void print_mtrx(int **mtrx);
+
 void find_available(int *W, int **alloc);
 int sum_vector(int *vec);
-int **read_mtrx(FILE *fp, char mode);
+
 int *copy_vector(int *SRC, int *DST);
-void print_mtrx(int **mtrx);
